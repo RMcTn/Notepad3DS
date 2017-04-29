20fe16a90cf8feee0abe709af813846cef9b4dd9
Reece <rmctiernan1@gmail.com>
2017-04-29 15:29:21 +0100

    Current line selection added to 'A' press, and keyboard cleanup
    Pressing A now selects the current line, outputs it to the keyboard
    and can be edited and saved.

--- source/main.cpp
+++ source/main.cpp
@@ -1,21 +1,37 @@
 #include <3ds.h>
 #include <stdio.h>
 #include <string.h>
+#include "file.h"
+#include "display.h"
 
+#define BUFFER_SIZE 60
 int main(int argc, char **argv)
 {
 	gfxInitDefault();
-	consoleInit(GFX_TOP, NULL);
-
+	consoleInit(GFX_TOP, &topScreen);
+    consoleInit(GFX_BOTTOM, &bottomScreen);
+    consoleSelect(&bottomScreen);
     //Software keyboard thanks to fincs
 	printf("Press A to select current line\n");
 	printf("Press B to clear screen\n");
 	printf("Press X to save file\n");
 	printf("Press Y to open file\n");
 	printf("Press START to exit\n");
+    
+    File file;      //Use as default file
+    unsigned int curr_line = 0;
+
+    consoleSelect(&topScreen);
+    update_screen(file, curr_line);
 
 	while (aptMainLoop())
 	{
+		static SwkbdState swkbd;
+		static char mybuf[BUFFER_SIZE];
+		static SwkbdStatusData swkbdStatus;
+		SwkbdButton button = SWKBD_BUTTON_NONE;
+		bool didit = false;
+
 		hidScanInput();
 
 		u32 kDown = hidKeysDown();
@@ -25,6 +41,22 @@ int main(int argc, char **argv)
 
         if (kDown & KEY_A) {
             //Select current line for editing
+            swkbdSetHintText(&swkbd, "Input text here.");
+            //File.lines iterator to find current selected line
+            auto line = file.lines.begin();
+            if (curr_line < file.lines.size())
+            {
+                if (curr_line != 0)
+                    advance(line, curr_line);
+                
+                //Need a char array to output to keyboard
+                char current_text[BUFFER_SIZE];
+                copy(line->begin(), line->end(), current_text);
+
+                swkbdSetInitialText(&swkbd, current_text);
+            }
+            didit = true;
+            button = swbkdInputText(&swkbd, mybuf, sizeof(mybuf));
         }
 
         if (kDown & KEY_B) {
@@ -47,18 +79,18 @@ int main(int argc, char **argv)
             //Move a line up (towards top of screen)
         }
 
-		static SwkbdState swkbd;
-		static char mybuf[60];
-		static SwkbdStatusData swkbdStatus;
-		SwkbdButton button = SWKBD_BUTTON_NONE;
-		bool didit = false;
 
 		if (didit)
 		{
 			if (button != SWKBD_BUTTON_NONE)
 			{
-				printf("You pressed button %d\n", button);
-				printf("Text: %s\n", mybuf);
+                std::vector<char> new_text = char_arr_to_vector(mybuf);
+                
+                if (curr_line >= file.lines.size())
+                    //Empty line, add a new one.
+                    file.add_line(new_text);
+                else
+                    file.edit_line(new_text, curr_line);
 			} else
 				printf("swkbd event: %d\n", swkbdGetResult(&swkbd));
 		}
